bede9d817680255ae8be7ebcf73f9b5e70993a5c
NerialSumber <gabrieloliveira31082000@gmail.com>
2026-04-09 17:09:10 -0300

    chore: fixing the structure of movie.repo

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,10 +1,22 @@
 import { NextResponse } from 'next/server'
-import { listMovies, createMovie } from '@/app/modules/movies/movies.repo'
+import { listMovies, createMovie, updateMovie, deleteMovie, getMovieById } from '@/app/modules/movies/movies.repo'
 
 export async function GET() {
   const movies = await listMovies()
 
   return NextResponse.json({ data: movies }, { status: 200 })
+}
+
+export async function GET_BY_ID(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const id = searchParams.get('id')
+  if (!id) {
+    return NextResponse.json({ error: 'ID is Mandatory' }, { status: 400 })
+  }
+
+  const movie = await getMovieById(id)
+
+  return NextResponse.json({ data: movie }, { status: 200 })
 }
 
 export async function POST(request: Request) {
@@ -13,4 +25,20 @@ export async function POST(request: Request) {
   const movie = await createMovie({ title, durationMinutes, ageRating, active })
 
   return NextResponse.json({ data: movie }, { status: 201 })
+}
+
+export async function UPDATE(request: Request) {
+  const body = await request.json()
+  const { id, title, durationMinutes, ageRating, active } = body
+  const updatedMovie = await updateMovie(id, { title, durationMinutes, ageRating, active })
+
+  return NextResponse.json({ data: updatedMovie }, { status: 200 })
+}
+
+export async function DELETE(request: Request) {
+  const body = await request.json()
+  const { id } = body
+  const deletedMovie = await deleteMovie(id)
+
+  return NextResponse.json({ data: deletedMovie }, { status: 200 })
 }
